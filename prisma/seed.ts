977bda76e86f1c4963d10de17d5c17a518520ab5
ritikverma127 <rv1338739@gmail.com>
2026-03-03 17:13:54 +0530

    change the seed file

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -32,7 +32,7 @@ async function main() {
     data: [
       {
         userId: adminUser.id,
-        days: [1, 2, 3, 4, 5], // Monday to Friday
+        days: "1,2,3,4,5", // Monday to Friday (comma-separated)
         startTime: "09:00",
         endTime: "17:00",
       },
